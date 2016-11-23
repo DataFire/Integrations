7ac3af7aef0e02169bf131066de49185a79966ad
Ubuntu <ubuntu@ip-10-0-0-198.us-west-2.compute.internal>
2016-11-23 16:22:11 +0000

    rename specs to integration.json

--- scripts/integrate.js
+++ scripts/integrate.js
@@ -1,16 +1,18 @@
 const fs = require('fs');
+const path = require('path');
 const datafire = require('datafire');
 const DIRECTORY = __dirname + '/../integrations';
 
 const TYPES = ['rss', 'openapi', 'raml', 'wadl', 'api_blueprint'];
 
 module.exports = (args, callback=()=>{}) => {
-  args.directory = DIRECTORY + '/' + args.name;
+  args.name = args.name.toLowerCase();
+  args.directory = DIRECTORY;
   let type = TYPES.filter(t => args[t])[0];
   datafire.commands.integrate(args, (e, spec) => {
     if (e) throw e;
     let pkg = JSON.parse(JSON.stringify(require('../package-template.json')))
-    let packageFile = args.directory + '/package.json';
+    let packageFile = path.join(args.directory, args.name, 'package.json');
     if (fs.existsSync(packageFile)) {
       pkg = require(packageFile);
     }
