7e09fa8d6cad0488c1c7662b1da537d97df73b84
Ubuntu <ubuntu@ip-10-0-0-198.us-west-2.compute.internal>
2016-11-23 20:22:22 +0000

    cut down on list fields

--- scripts/generate-list.js
+++ scripts/generate-list.js
@@ -7,6 +7,10 @@ const truncateDescription = (desc) => {
   if (!desc) return;
   desc = desc || '';
   desc = desc.replace(/<(?:.|\n)*?>/gm, '');
+  let newLine = desc.indexOf('\n');
+  if (newLine > -1) {
+    desc = desc.substring(0, newLine);
+  }
   if (desc.length > MAX_DESCRIPTION_LENGTH) {
     desc = desc.substring(0, MAX_DESCRIPTION_LENGTH) + '...';
   }
@@ -16,7 +20,10 @@ const truncateDescription = (desc) => {
 let list = {};
 fs.readdirSync(__dirname + '/../integrations').forEach(name => {
   let integration = datafire.Integration.new(name);
-  list[name] = integration.spec.info;
+  list[name] = {
+    title: integration.spec.info.title,
+    description: integration.spec.info.description,
+  }
   list[name].description = truncateDescription(list[name].description);
 })
 fs.writeFileSync(__dirname + '/../list.json', JSON.stringify(list, null, 2));
