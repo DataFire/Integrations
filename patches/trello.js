0a4e8ae9b426a8979f2a1ae9e11b0bee45065eed
Ubuntu <ubuntu@ip-10-0-0-198.us-west-2.compute.internal>
2016-12-07 21:14:54 +0000

    fix trello key location

--- patches/trello.js
+++ patches/trello.js
@@ -1,5 +1,6 @@
 module.exports = (spec) => {
   spec.securityDefinitions.api_key.name = 'key';
+  spec.securityDefinitions.api_key.in = 'query';
   spec.securityDefinitions.api_token = {
     type: 'apiKey',
     in: 'query',
