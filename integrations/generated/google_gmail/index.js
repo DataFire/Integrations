80a45ecec5a99aebfd46a3713c4935211f215540
Travis CI <bobby.brennan@gmail.com>
2017-05-01 20:29:50 +0000

    add buildMessage action to gmail

--- integrations/generated/google_gmail/index.js
+++ integrations/generated/google_gmail/index.js
@@ -1,3 +1,37 @@
 let datafire = require('datafire');
 let openapi = require('./openapi.json');
 module.exports = datafire.Integration.fromOpenAPI(openapi, "google_gmail");
+
+module.exports.addAction('buildMessage', new datafire.Action({
+  inputSchema: {
+    type: 'object',
+    properties: {
+      to: {type: 'string'},
+      from: {type: 'string'},
+      subject: {type: 'string', default: ''},
+      body: {type: 'string', default: ''},
+    }
+  },
+  handler: (input, context) => {
+    let message = `
+
+From: <${input.from}>
+To: <${input.to}>
+Subject: ${input.subject}
+Date: ${new Date().toString()}
+Content-Type: text/html; charset=utf-8
+
+<html>
+  <body>
+${input.body}
+  </body>
+</html>
+
+    `.trim();
+
+    let encoded = new Buffer(message).toString('base64');
+    encoded = encoded.replace(/\//g,'_').replace(/\+/g,'-').replace(/=+$/, '');
+    return encoded;
+  }
+
+}))
