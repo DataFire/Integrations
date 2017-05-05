f2dec14cf00547394434fb5b17ecddd753751da2
Travis CI <bobby.brennan@gmail.com>
2017-05-05 16:37:44 +0000

    add JSON descriptions for each integ/action

--- scripts/generate-list.js
+++ scripts/generate-list.js
@@ -1,8 +1,17 @@
 const fs = require('fs');
 const path = require('path');
+const mkdirp = require('mkdirp');
 const datafire = require('datafire');
 
 const MAX_DESCRIPTION_LENGTH = 120;
+
+const maybeMkdirp = (dir) => {
+  try {
+    mkdirp.sync(dir);
+  } catch (e) {
+    if (e.code !== 'EEXIST') throw e;
+  }
+}
 
 const truncateDescription = (desc) => {
   if (!desc) return;
@@ -18,22 +27,43 @@ const truncateDescription = (desc) => {
   return desc;
 }
 
+const BASE_DIR = __dirname + '/../integrations/';
+const OUT_DIR = __dirname + '/../json/';
+
 let list = {};
 function addDirToList(dir) {
   fs.readdirSync(dir).forEach(name => {
     console.log('adding', name);
     let integ = require(path.join(dir, name));
+    let package = require(path.join(dir, name, 'package.json'));
     if (list[name]) throw new Error("Duplicate name " + name);
     list[name] = {
       title: integ.title,
       description: integ.description,
     };
+    let details = Object.assign({}, list[name]);
     list[name].description = truncateDescription(list[name].description);
+
+    details.actions = integ.allActions.map(action => {
+      details.definitions = details.definitions || action.inputSchema.definitions;
+      let actionDetails = {
+        id: action.id.split('/')[1],
+        title: action.title,
+        description: action.description,
+        inputSchema: Object.assign({definitions: null}, action.inputSchema),
+        outputSchema: Object.assign({definitions: null}, action.outputSchema),
+      }
+      delete actionDetails.inputSchema.definitions;
+      delete actionDetails.outputSchema.definitions;
+      return actionDetails;
+    })
+    let integDir = path.join(OUT_DIR, name, package.version);
+    maybeMkdirp(integDir);
+    fs.writeFileSync(path.join(integDir, 'index.json'), JSON.stringify(details, null, 2));
   })
 }
 
-const BASE_DIR = __dirname + '/../integrations/';
 addDirToList(BASE_DIR + 'generated');
 addDirToList(BASE_DIR + 'manual');
-fs.writeFileSync(__dirname + '/../list.json', JSON.stringify(list, null, 2));
+fs.writeFileSync(OUT_DIR + 'list.json', JSON.stringify(list, null, 2));
 
