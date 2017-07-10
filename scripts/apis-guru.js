297b7ba3d0c303741565a2e7cb02440b7463dca4
Travis CI <bobby.brennan@gmail.com>
2017-07-10 21:56:48 +0000

    add --new option for apis-guru

--- scripts/apis-guru.js
+++ scripts/apis-guru.js
@@ -1,6 +1,9 @@
 const request = require('request');
 const async = require('async');
+const fs = require('fs');
 const integrate = require('./integrate');
+
+const args = require('yargs').argv;
 
 const APIS_GURU_URL = "https://api.apis.guru/v2/list.json";
 
@@ -17,26 +20,42 @@ const NAME_CHANGES = {
   "posty-api.herokuapp.com": "posty"
 }
 
+const getName = (apisGuruName) => {
+  let provider = '';
+  let name = NAME_CHANGES[apisGuruName];
+  if (!name) {
+    name = apisGuruName.substring(apisGuruName.indexOf(':') + 1);
+    provider = apisGuruName.substring(0, apisGuruName.indexOf(':'));
+    provider = NAME_CHANGES[provider] || provider;
+    SUFFIXES.forEach(suffix => {
+      let regex = new RegExp(suffix.replace(/\./g, '\\.') + '$');
+      name = name.replace(regex, '');
+      provider = provider.replace(regex, '');
+    });
+    if (provider) name = provider + '_' + name;
+  }
+  name = name.toLowerCase().replace(/\W+/g, '_');
+  return {provider, name};
+}
+
+const OUT_DIR = __dirname + '/../integrations/generated';
+
 request.get(APIS_GURU_URL, {json: true}, (err, resp, body) => {
   if (err) throw err;
-  async.parallel(Object.keys(body).map(key => {
+  let keys = Object.keys(body);
+  if (args.new) {
+    keys = keys.filter(key => {
+      let {provider, name} = getName(key);
+      let e = !fs.existsSync(OUT_DIR + '/' + name);
+      return e;
+    })
+  }
+  async.parallel(keys.map(key => {
     return acb => {
       let api = body[key];
       let version = api.preferred + '.0';
       api = api.versions[api.preferred];
-      let name = NAME_CHANGES[key];
-      let provider = '';
-      if (!name) {
-        name = key.substring(key.indexOf(':') + 1);
-        provider = key.substring(0, key.indexOf(':'));
-        provider = NAME_CHANGES[provider] || provider;
-        SUFFIXES.forEach(suffix => {
-          let regex = new RegExp(suffix.replace(/\./g, '\\.') + '$');
-          name = name.replace(regex, '');
-          provider = provider.replace(regex, '');
-        });
-        if (provider) name = provider + '-' + name;
-      }
+      let {name, provider} = getName(key);
       integrate({
         name,
         version,
