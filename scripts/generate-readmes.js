2c097a0610b7fbc125e14080aada9bf6cd78fd81
Travis CI <bobby.brennan@gmail.com>
2017-07-10 21:42:58 +0000

    add generate-readmes script

---
+++ scripts/generate-readmes.js
@@ -0,0 +1,23 @@
+let path = require('path');
+let fs = require('fs');
+let iterateIntegs = require('./iterate-integrations');
+
+function schemaToMarkdown(schema, title) {
+  if (!schema) return '';
+  delete schema.definitions;
+  return `### ${title || ''} Schema\n` + '```json\n' + JSON.stringify(schema, null, 2) + '\n```\n'
+}
+
+iterateIntegs((dir, name, integ) => {
+  let md = '# @datafire/' + integ.id + '\n';
+  md += integ.description + '\n\n';
+  integ.allActions.forEach(action => {
+    let id = action.id.substring(action.id.indexOf('/') + 1);
+    md += '## Operation: ' + id + '\n' + action.description + '\n\n';
+    md += schemaToMarkdown(action.inputSchema, 'Input');
+    md += schemaToMarkdown(action.outputSchema, 'Output');
+  })
+  let readmeFile = path.join(dir, 'README.md');
+  fs.writeFileSync(readmeFile, md);
+})
+
--- scripts/integrate.js
+++ scripts/integrate.js
@@ -5,12 +5,6 @@ const DESTINATION = __dirname + '/../integrations/generated';
 
 const TYPES = ['rss', 'openapi', 'raml', 'wadl', 'api_blueprint'];
 
-function schemaToMarkdown(schema, title) {
-  if (!schema) return '';
-  delete schema.definitions;
-  return `### ${title || ''} Schema\n` + '```json\n' + JSON.stringify(schema, null, 2) + '\n```\n'
-}
-
 module.exports = (args, callback=()=>{}) => {
   args.name = args.name.toLowerCase().replace(/\W+/g, '_');
   args.destination = DESTINATION;
@@ -19,33 +13,13 @@ module.exports = (args, callback=()=>{}) => {
     console.log('Writing', args.name);
     let spec = require(path.join(args.destination, args.name, 'openapi.json'));
     let pkg = JSON.parse(JSON.stringify(require('../package-template.json')))
-    let packageFile = path.join(args.destination, args.name, 'package.json');
     pkg.name = '@datafire/' + args.name;
     pkg.description = "DataFire integration for " + (spec.info.title || args.name);
     pkg.datafire = pkg.datafire || {};
     pkg.datafire.origin = args[type];
     pkg.datafire.type = type;
-    let integ = require(path.join(args.destination, args.name, 'index.js'));
-    let md = '# @datafire/' + integ.id + '\n';
-    md += integ.description + '\n\n';
-    integ.allActions.forEach(action => {
-      let id = action.id.substring(action.id.indexOf('/') + 1);
-      md += '## Operation: ' + id + '\n' + action.description + '\n\n';
-      md += schemaToMarkdown(action.inputSchema, 'Input');
-      md += schemaToMarkdown(action.outputSchema, 'Output');
-    })
-    let readmeFile = path.join(args.destination, args.name, 'README.md');
-    fs.writeFileSync(readmeFile, md);
+    let packageFile = path.join(args.destination, args.name, 'package.json');
     fs.writeFileSync(packageFile, JSON.stringify(pkg, null, 2));
-    if (integ.ajv) {
-      integ.ajv._cache.clear();
-    }
-    integ = null;
-    for (let key in require.cache) {
-      if (key.indexOf(path.join(__dirname, '..')) !== -1) {
-        delete require.cache[key];
-      }
-    }
     callback();
   }
   if (process.env.NO_UPDATE && fs.existsSync(path.join(args.destination, args.name))) {
