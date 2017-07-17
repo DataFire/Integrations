6cffa0ef086b42d92db877ea4ad4ad374b1a5887
Travis CI <bobby.brennan@gmail.com>
2017-07-17 15:07:57 +0000

    lucybot_docs: serve project.openapi

--- integrations/manual/lucybot_docs/index.js
+++ integrations/manual/lucybot_docs/index.js
@@ -13,12 +13,39 @@ let serveContext = new datafire.Context({
   }
 })
 
+
+let openapi = null;
+
+function initializeOpenAPI() {
+  if (openapi) return openapi;
+  let project = datafire.Project.main();
+  openapi = JSON.parse(JSON.stringify(project.openapi));
+
+  for (let path in openapi.paths) {
+    for (let method in openapi.paths[path]) {
+      let op = openapi.paths[path][method];
+      if (!op['x-datafire']) return;
+      if (op['x-datafire'].action === 'lucybot_docs/serve') {
+        delete openapi.paths[path][method];
+      }
+    }
+    if (!Object.keys(openapi.paths[path]).length) {
+      delete openapi.paths[path];
+    }
+  }
+}
+
 lucybot.addAction('serve', {
   input: [{
     title: 'filename',
     type: 'string',
   }],
-  handler: (input) => {
-    return fileserver.serve(input, serveContext);
+  handler: (input, context) => {
+    initializeOpenAPI();
+    if (input.filename === 'openapi.json') {
+      return openapi;
+    } else {
+      return fileserver.serve(input, serveContext);
+    }
   }
 })
