b23f16f380b6ae5491adab1918ebd14b5117e685
Travis CI <bobby.brennan@gmail.com>
2017-07-19 16:17:31 +0000

    squareup: remove extra auth parameter

---
+++ patches/squareup.js
@@ -0,0 +1,9 @@
+module.exports = spec => {
+  for (let path in spec.paths) {
+    for (let method in spec.paths[path]) {
+      let op = spec.paths[path][method];
+      if (!op.parameters) continue;
+      op.parameters = op.parameters.filter(p => p.name !== 'Authorization');
+    }
+  }
+}
