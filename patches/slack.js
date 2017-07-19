158d2fc15e74c43b2b7205463a6cc66a28ab2716
Travis CI <bobby.brennan@gmail.com>
2017-07-19 16:02:39 +0000

    add x-location to slack security def

--- patches/slack.js
+++ patches/slack.js
@@ -1,5 +1,9 @@
 module.exports = spec => {
-  spec.securityDefinitions.oauth_2_0.name = 'token';
+  let oauth = spec.securityDefinitions.oauth_2_0;
+  oauth['x-location'] = {
+    name: 'token',
+    in: 'query',
+  }
   for (let path in spec.paths) {
     let op = spec.paths[path].get;
     op.operationId = path.substring(1).replace(/\.(\w)/, (match, w) => w.toUpperCase());
