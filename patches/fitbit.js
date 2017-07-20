8bbee46d92fe3d7b279c0d9536b65ede3a8893d6
Travis CI <bobby.brennan@gmail.com>
2017-07-20 22:13:53 +0000

    update fitbit patch

--- patches/fitbit.js
+++ patches/fitbit.js
@@ -2,4 +2,12 @@ module.exports = spec => {
   let def = spec.securityDefinitions.oauth2;
   def.tokenUrl = 'https://api.fitbit.com/oauth2/token';
   def.authorizationUrl = 'https://www.fitbit.com/oauth2/authorize';
+  for (let path in spec.paths) {
+    if (path.match(/\?.*/)) delete spec.paths[path];
+    /*let newPath = path.replace(/\?.*$/, '');
+    if (newPath === path) continue;
+    spec.paths[newPath] = spec.paths[path];
+    delete spec.paths[path];*/
+  }
+  require('./util/remove-parameter')(spec, 'format');
 }
--- patches/util/remove-parameter.js
+++ patches/util/remove-parameter.js
@@ -1,5 +1,8 @@
 module.exports = (spec, filterName) => {
   for (let path in spec.paths) {
+    if (spec.paths[path].parameters) {
+      spec.paths[path].parameters = spec.paths[path].parameters.filter(p => p.name !== filterName);
+    }
     for (let method in spec.paths[path]) {
       let op = spec.paths[path][method];
       if (!op.parameters) continue;
