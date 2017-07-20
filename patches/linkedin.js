5ba81105cf58825e066d0837c873bec6f841de5d
Travis CI <bobby.brennan@gmail.com>
2017-07-20 21:45:04 +0000

    new linkedin patch

--- patches/linkedin.js
+++ patches/linkedin.js
@@ -2,4 +2,17 @@ module.exports = spec => {
   let def = spec.securityDefinitions.OAuth;
   def.tokenUrl = 'https://www.linkedin.com/oauth/v2/accessToken';
   def.authorizationUrl = 'https://www.linkedin.com/oauth/v2/authorization';
+  for (let path in spec.paths) {
+    let newPath = path.replace(/\?format=json.*/, '');
+    if (newPath === path) continue;
+    let ops = spec.paths[newPath] = spec.paths[path];
+    delete spec.paths[path];
+    let params = ops.parameters = ops.parameters || [];
+    params.push({
+      name: 'format',
+      in: 'query',
+      type: 'string',
+      'x-consoleDefault': 'json',
+    })
+  }
 }
