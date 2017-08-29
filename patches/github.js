6cadb87ee3db77366570e554a60e7219e958e193
Travis CI <bobby.brennan@gmail.com>
2017-08-29 19:12:47 +0000

    github: remove header parameters

--- patches/github.js
+++ patches/github.js
@@ -1,3 +1,5 @@
+let removeParam = require('./util/remove-parameter');
+
 module.exports = (spec) => {
   for (let path in spec.paths) {
     let op = spec.paths[path].get;
@@ -9,4 +11,11 @@ module.exports = (spec) => {
       })
     }
   }
+  spec.definitions.postGist.properties.files.additionalProperties = true;
+  removeParam(spec, 'X-GitHub-Media-Type');
+  removeParam(spec, 'Accept');
+  removeParam(spec, 'X-RateLimit-Limit');
+  removeParam(spec, 'X-RateLimit-Remaining');
+  removeParam(spec, 'X-RateLimit-Reset');
+  removeParam(spec, 'X-GitHub-Request-Id');
 }
