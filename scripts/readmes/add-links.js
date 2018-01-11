22a8a4180558284a8352ff6d82441cb94cb5dc8c
Travis CI <bobby.brennan@gmail.com>
2018-01-11 22:00:20 +0000

    remove link from readme

--- scripts/readmes/add-links.js
+++ scripts/readmes/add-links.js
@@ -3,6 +3,8 @@ const TITLE_LINE = '## Integrations';
 const README_FILE = __dirname + '/../../README.md';
 const removeMD = require('remove-markdown-and-html');
 
+const SKIP = ['apitore_twitterapiswithword2vecqueryexpansion_andaddsentimentbyapitoresentimentapi'];
+
 let readme = fs.readFileSync(README_FILE, 'utf8');
 let list = require('../../json/list');
 
@@ -16,6 +18,7 @@ readme += '\n|name|title|README|link|description|';
 readme += '\n|---|---|---|---|---|';
 
 Object.keys(list).sort().forEach(key => {
+  if (SKIP.indexOf(key) !== -1) return;
   let details = list[key];
   let parts = [
     key,
