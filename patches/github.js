7ddd2d998fad8b6d26a559dca2bfc4f1fcea597f
Bobby Brennan <bobby.brennan@gmail.com>
2018-06-12 11:30:58 +0000

    github: fewer required parameters

--- patches/github.js
+++ patches/github.js
@@ -2,13 +2,17 @@ let removeParam = require('./util/remove-parameter');
 
 module.exports = (spec) => {
   for (let path in spec.paths) {
-    let op = spec.paths[path].get;
-    if (op && path.endsWith('s')) {
-      op.parameters.push({
-        name: 'page',
-        in: 'query',
-        type: 'integer',
-      })
+    for (let method in spec.paths[path]) {
+      let op = spec.paths[path][method];
+      if (method === 'get' && path.endsWith('s')) {
+        op.parameters.push({
+          name: 'page',
+          in: 'query',
+          type: 'integer',
+        })
+      }
+      // Lots of parameters that are optional are listed as required
+      (op.parameters || []).filter(p => p.in !== 'path' && p.in !== 'body').forEach(p => delete p.required);
     }
   }
   spec.definitions.postGist.properties.files.additionalProperties = true;
