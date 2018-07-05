166ce881e225524572d3bdc2e6cc3a802f077123
Bobby Brennan <bobby.brennan@gmail.com>
2018-07-05 18:25:13 +0000

    xero: enable oauth 1

--- integrations/generated/xero/index.js
+++ integrations/generated/xero/index.js
@@ -1,4 +1,22 @@
 "use strict";
 let datafire = require('datafire');
 let openapi = require('./openapi.json');
-module.exports = datafire.Integration.fromOpenAPI(openapi, "xero");
+let xero = module.exports = datafire.Integration.fromOpenAPI(openapi, "xero", function(reqOpts, context) {
+  reqOpts.oauth = {
+    consumer_key: context.accounts.xero.consumer_key,
+    consumer_secret: context.accounts.xero.consumer_secret,
+    token: context.accounts.xero.token,
+    token_secret: context.accounts.xero.token_secret,
+  }
+});
+
+xero.security = {
+  xero: {
+    fields: {
+      consumer_key: '',
+      consumer_secret: '',
+      token: '',
+      token_secret: '',
+    }
+  }
+}
